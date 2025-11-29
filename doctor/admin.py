00bc2d68bf3f98b950a5c26c3b51f5c5199c3204
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-11-30 00:18:46 +0600

    Update admin.py

--- doctor/admin.py
+++ doctor/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.AvailableTime)
+
+class SpecialaztionAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title")}
+
 admin.site.register(models.Designation)
-admin.site.register(models.Specialization)
+
+
+admin.site.register(models.Specialization,SpecialaztionAdmin )
 admin.site.register(models.Review)
 admin.site.register(models.Doctor)
