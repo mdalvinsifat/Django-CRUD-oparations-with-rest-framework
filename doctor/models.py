4f04f662f0b900aa1d7141dd974207a8e6ece498
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-11-30 00:14:35 +0600

    Update models.py

--- doctor/models.py
+++ doctor/models.py
@@ -2,16 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from patient.models import Patient
 # Create your models here.
+
+
+
+# Dermatologist 
+# Genarel Surgary 
+# Neurologist 
+
 class Specialization(models.Model):
     name = models.CharField(max_length = 30)
     slug = models.SlugField(max_length = 40)
     def __str__(self):
         return self.name
+
+
+    # Senior Consultant 
+    # Junior Consultant 
+    # Medical Officer Consultant 
+
 class Designation(models.Model):
     name = models.CharField(max_length = 30)
     slug = models.SlugField(max_length = 40)
     def __str__(self):
             return self.name
+
+            
 class AvailableTime(models.Model):
     name = models.CharField(max_length = 100)
     def __str__(self):
